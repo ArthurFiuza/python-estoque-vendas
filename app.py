b91cadcafda0ed468213817b917d9b25de200654
Arthur Fiuza Guimaraes <arthur.guimaraes@crpmg.local>
2026-04-28 17:58:13 -0300

    Criação da estrutura inicial do arquivo de produtos CSV

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
+import csv
+import os
+
+def criar_arquivos_produtos():
+    if not os.path.exists("produtos.csv"):
+        with open("produtos.csv", "w", newline="", encoding="utf-8") as arquivo:
+            writer = csv.writer(arquivo)
+            
+            writer.writerow([
+                "id_produto",
+                "nome",
+                "categoria",
+                "quantidade",
+                "preco",
+                "fornecedor",
+                "data_adicao"
+            ])
+  
+
 def menu():
     print("Sistema de Estoque")
+
     
+criar_arquivos_produtos()
 menu()
+
